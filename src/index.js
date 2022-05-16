49743f8cf989cf2a0e4d9007f0e9df34d2ffc1d9
uisproject <uisprojectxyz@gmail.com>
2022-05-16 16:33:30 +0700

    tailwind installed

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import ShoppingPage from './ShoppingPage'
-import MyCartPage from './MyCartPage'
-import ProfilePage from './ProfilePage'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import ShoppingPage from "./ShoppingPage";
+import MyCartPage from "./MyCartPage";
+import ProfilePage from "./ProfilePage";
+import "./styles/index.css";
 
 const App = () => {
-    return(
-        <div style={{
-            maxWidth:'400px',
-            height:'calc(100vh + 120px)',
-            minHeight:'calc(100vh - 100px)',
-            margin:'auto'
-        }}>
-            <ShoppingPage />
-        </div>
-    )
-}
+  return (
+    <div
+      style={{
+        maxWidth: "400px",
+        height: "calc(100vh + 120px)",
+        minHeight: "calc(100vh - 100px)",
+        margin: "auto",
+      }}
+    >
+      <ShoppingPage />
+    </div>
+  );
+};
 
-ReactDOM.render(<App />,document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
